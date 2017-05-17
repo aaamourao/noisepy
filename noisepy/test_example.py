ef111b59d84093caab52a60c1386ab43e2fe4ffa
deam0nd <adrianomourao@protonmail.com>
2017-05-17 15:38:43 -0300

    Adding test example for fast and quick debugging on development

---
+++ noisepy/test_example.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+#     ____ _     ___ _____ ____ _   _      _      ____    _____     _  _
+#    / ___| |   |_ _|_   _/ ___| | | |    / \    |  _ \  |_   _|  _| || |
+#   | |  _| |    | |  | || |   | |_| |   / _ \   | |_) |   | |   |_  ..  _|
+#   | |_| | |___ | |  | || |___|  _  |  / ___ \  |  _ <    | |   |_      _|
+#    \____|_____|___| |_| \____|_| |_| /_/   \_\ |_| \_\   |_|     |_||_|
+#
+# by
+#      ___          ___         _____         ___          ___          ___
+#     /__/\        /  /\       /  /::\       /  /\        /  /\        /__/\
+#    |  |::\      /  /::\     /  /:/\:\     /  /:/       /  /::\      _\_ \:\
+#    |  |:|:\    /  /:/\:\   /  /:/  \:\   /  /:/       /  /:/\:\    /__/\ \:\
+#  __|__|:|\:\  /  /:/~/::\ /__/:/ \__\:| /  /:/  ___  /  /:/  \:\  _\_ \:\ \:\
+# /__/::::| \:\/__/:/ /:/\:\\  \:\ /  /://__/:/  /  /\/__/:/ \__\:\/__/\ \:\ \:\
+# \  \:\~~\__\/\  \:\/:/__\/ \  \:\  /:/ \  \:\ /  /:/\  \:\ /  /:/\  \:\ \:\/:/
+#  \  \:\       \  \::/       \  \:\/:/   \  \:\  /:/  \  \:\  /:/  \  \:\ \::/
+#   \  \:\       \  \:\        \  \::/     \  \:\/:/    \  \:\/:/    \  \:\/:/
+#    \  \:\       \  \:\        \__\/       \  \::/      \  \::/      \  \::/
+#     \__\/        \__\/                     \__\/        \__\/        \__\/
+#
+#  glitchefx.py -- This belongs to noisepy
+#
+#  noisepy is a python module which implements methods for creating glitch art
+#  pictures and movies
+#
+#  Copyright (C) 2017 madc0w - adriano mourao <madc0w@protonmail.ch>
+#
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the Free
+#  Software Foundation; either version 3, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+#  for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import core
+
+print("Initializing...")
+effects = core.EffxHist("../images/barcelona.jpg")
+print("Creating effects...")
+effects.enqueue('Amp', gain=1.6, channels='g')
+effects.enqueue('Inv', channels='rb')
+effects.enqueue('GrayScale')
+print("Processing...")
+effects.executeQueue()
+print("Saving Image...")
+effects.save("../glitched/glitched_gs_barcelona.jpg")
+print("Done!")
