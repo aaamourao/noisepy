df18128b9712f7b90b9c12b69b56395df9817ef7
deam0nd <adrianomourao@protonmail.com>
2017-05-20 01:23:42 -0300

    Starting module refactoring

--- noisepy/util/termint.py
+++ noisepy/util/termint.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 #     ____ _     ___ _____ ____ _   _      _      ____    _____     _  _
 #    / ___| |   |_ _|_   _/ ___| | | |    / \    |  _ \  |_   _|  _| || |
 #   | |  _| |    | |  | || |   | |_| |   / _ \   | |_) |   | |   |_  ..  _|
@@ -37,21 +36,28 @@
 #  You should have received a copy of the GNU General Public License along
 #  with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+"""
+Terminal Interface module:
+    Does the argument parsing before create effect object. It is not
+    necessary to create a EffxBoard instance, since only a single
+    effect will be applied.
+"""
 import os.path
 import argparse
-import imageio
-import glitchefx
+import sys
+import util.effx
 
-class NoisePy(object):
-    """
-    Noisepy Class:
-     * Implemented using the command pattern
-     * Represents an edited image and the effects
-    """
+class TermInt(object):
     def __init__(self):
+        # TODO: Command Pattern
+        print('Hey, implement me, please')
+
+    @classmethod
+    def commandLine():
         """
-        Manual glitching interface: Check user inputs, open,
-        create, and save images as numpy arrays...
+        commandLine is a method which implements the
+        common steps that every effect should do when
+        called from the command line.
         """
         # Create argument parser object
         parser = argparse.ArgumentParser(
@@ -60,46 +66,42 @@ class NoisePy(object):
                         ' ===noisepy=== by ___madc0w___ !' \
                         ' Distributed under GNU GPL v3.' \
                         ' Check COPYING file for more information')
+        # All effects must-have arguments
+        parser.add_argument("infile", help="Target image path")
+        parser.add_argument("outfile", help="Output file path")
+
+        # Parse arguments
+        setup = parser.parse_args()
+        # Check if input image exists and has permission
+        try:
+            if not os.path.isfile(setup.input_path):
+                raise ValueError('Could not access image file. ' \
+                        'If it exists, check its permissions')
+        except ValueError as message:
+            print(message)
+        # Check if parameters were specified
+        if not setup.colors:
+            setup.colors = 'rgb'
+
+        """
+        Create subparsers for subcommands as a class attribute
+        """
+        self.subparsers = parser.add_subparsers(help='Effect to process', \
+                dest="command")
+
         # Check if the requested command exists
-        parser.add_argument('command', help='Subcommand to run')
-        args = parser.parse_args(sys.args[1:2])
-        if not hasattr(self, args.command):
-            print 'Unrecognized command'
+        if not hasattr(self, subparsers.command):
+            print('Unrecognized command')
             parser.print_help()
             exit(1)
-        # Call the requested effect
-        getattr(self, args.command)()
 
-    def effect(func):
-        """
-        Effect is a decorating method which implements the
-        common steps that every effect should do.
-        """
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            # Parse arguments
-            setup = parser.parse_args()
-            # Check if input image exists and has permission
-            try:
-                if not os.path.isfile(setup.input_path):
-                    raise ValueError('Could not access image file. ' \
-                            'If it exists, check its permissions')
-            except ValueError as message:
-                print(message)
-            # Check if parameters were specified
-            if not setup.colors:
-                setup.colors = 'rgb'
+        # Load image in a numpy array
+        signal = imageio.imread(setup.input_path)
+        # Compute the requested signal processing function
+        proc_sig = getattr(self, str(self.subparsers.command))()
+        # Write the output image on target file
+        imageio.imwrite(setup.output_path, proc_sig, 'jpg')
 
-            # Load image in a numpy array. It will contain a matrix with
-            # an element for each picture. Each element is a 3 dimensional
-            # vector. It will be treated it as a 3 channels signal.
-            signal = imageio.imread(setup.input_path)
-            # Compute the requested signal processing
-            processed = func(*args, **kwargs)
-            # Write the output image on target file
-            imageio.imwrite(setup.output_path, output, 'jpg')
-
-    @effect
     def amplifier(self):
         """
         Amplifier implements the amplify processing on a
@@ -107,26 +109,23 @@ class NoisePy(object):
         So it sets its help parameter table and check if they were
         passed corretly.
         """
-        # Create sub-parsers for sub-commands
-        subparsers = parser.add_subparsers(help='sub-command help',
-                dest="command")
 
         # Create parser for "amplifier" command
-        parser_amp = subparsers.add_parser("amplifier", \
+        parser_amp = self.subparsers.add_parser("amplifier", \
                 help="Select amplifier signal edition")
         parser_amp.add_argument("-a", "--ampgain", type=float,
                 help="Amplifier gain")
         parser_amp.add_argument("-c", "--colors", nargs='+',
                 choices=['r', 'g', 'b'],
                 help="Color plane(s) that will be edited: RGB")
-        parser_amp.add_argument("input_path", help="Target image path")
-        parser_amp.add_argument("output_path", help="Output file path")
 
-        # Check if there are required parameters
+        # Check if there are amplifier specific required parameters
         if not setup.ampgain:
             parser.error("--ampgain is required")
 
-    @effect
+        # TODO: Call effect and return result
+        return 0
+
     def inversor(self):
         """
         Inversor implements a simple color inversion when called as
@@ -141,14 +140,8 @@ class NoisePy(object):
         parser_inv.add_argument("input_path", help="Target image path")
         parser_inv.add_argument("output_path", help="Output file path")
 
-    def preProcessor(self):
-        if setup.command == 'amplifier':
-            output = amplify(setup.ampgain, setup.colors, signal)
-        elif setup.command == 'inversor':
-            output = invert(setup.colors, signal)
-
 if __name__ == "__main__":
     """
     When executed from the command line
     """
-    NoisePy()
+    NoisePy.commandLine()
